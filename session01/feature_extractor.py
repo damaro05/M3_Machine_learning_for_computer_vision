b4dc2e00dbaa837c879e974338712a70020c025e
mkmenta <mkmenta95@gmail.com>
2017-12-15 11:18:43 +0100

    SURF and HOG added

--- session01/feature_extractor.py
+++ session01/feature_extractor.py
@@ -4,12 +4,17 @@ import sys
 import os
 import pickle #module for serialization of python object structure
 import gzip   #we can compress our data directly when writting data to a file
+from skimage.feature import hog
 
 class FeatureExtractor:
     def __init__(self, method):
         self.method=method
         if method == 'sift':
             self.f = self._sift_features
+        elif method == 'surf':
+            self.f = self._surf_features
+        elif method == 'hog':
+            self.f = self._hog_features
         else:
             print('Feature extracting method not found')
             sys.exit(-1)
@@ -22,9 +27,29 @@ class FeatureExtractor:
         print(str(len(kpt)) + ' extracted keypoints and descriptors')
         return des
 
-    def extract_features(self, train_images_filenames, train_labels, nimmax=float('inf'),picklepath='../../FeaturePickles'):
+    def _surf_features(self, ima):
+        SURFdetector = cv2.xfeatures2d.SURF_create(400)
+        gray = cv2.cvtColor(ima, cv2.COLOR_BGR2GRAY)
+        kpt, des = SURFdetector.detectAndCompute(gray, None)
+        print(str(len(kpt)) + ' extracted keypoints and descriptors')
+        return des
+
+    def _hog_features(self, ima):
+        #hog = cv2.HOGDescriptor()
+        #h = hog.compute(ima)
+        gray = cv2.cvtColor(ima, cv2.COLOR_BGR2GRAY)
+        #from skimage import exposure
+        #import matplotlib.pyplot as plt
+        #fd, hog_image = hog(gray, visualise=True,pixels_per_cell=(16,16))
+        #hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
+        #plt.imshow(hog_image_rescaled, cmap=plt.cm.gray)
+
+        fd = hog(gray,pixels_per_cell=(16,16)) # Adjust pixels_per_cell by a K-fold
+        return fd[np.newaxis]
+
+    def extract_features(self, train_images_filenames, train_labels, nimmax=float('inf'),picklepath='../../FeaturePickles', force_reload=False):
         # If features were previously computed, load them directly
-        if os.path.exists(picklepath+'/'+self.method+'.pklz'):
+        if os.path.exists(picklepath+'/'+self.method+'.pklz') and not force_reload:
             with gzip.open(picklepath+'/'+self.method+'.pklz', 'rb') as f:
                 (D, L) = pickle.load(f)
                 return (D, L)
