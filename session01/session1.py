714750e7a7e651a26e97ce17aab9fdab163a23a0
mkmenta <mkmenta95@gmail.com>
2017-12-15 09:52:09 +0100

    Modified the loading of precomputed labels

--- session01/session1.py
+++ session01/session1.py
@@ -1,4 +1,3 @@
-import cv2
 import numpy as np
 import time
 import data_loader
@@ -8,63 +7,64 @@ import os.path
 from feature_extractor import FeatureExtractor
 from classifier import Classifier
 
-start = time.time()
+#######################
+force_reload = False
+feature_method = 'sift'
+classifier = 'knn'
+#######################
 
 # Input images and labels
-(train_images_filenames,train_labels)=data_loader.load_input_metadata('train_images_filenames.dat', 'train_labels.dat')
-(test_images_filenames,test_labels)=data_loader.load_input_metadata('test_images_filenames.dat', 'test_labels.dat')
+(train_images_filenames, train_labels) = data_loader.load_input_metadata('train_images_filenames.dat',
+                                                                         'train_labels.dat')
+(test_images_filenames, test_labels) = data_loader.load_input_metadata('test_images_filenames.dat', 'test_labels.dat')
 
-print('Loaded '+str(len(train_images_filenames))+' training images filenames with classes ',set(train_labels))
-print('Loaded '+str(len(test_images_filenames))+' testing images filenames with classes ',set(test_labels))
+print('Loaded ' + str(len(train_images_filenames)) + ' training images filenames with classes ', set(train_labels))
+print('Loaded ' + str(len(test_images_filenames)) + ' testing images filenames with classes ', set(test_labels))
 
-# Feature extraction
-fe=FeatureExtractor('sift')
-(D,L)=fe.extract_features(train_images_filenames,train_labels,nimmax=30)
-
-# Train a classifier
-c=Classifier('knn')
-c.fit(D,L)
-filename = c.name + '.npy'
-#c.__dict__.keys()
-
-# Predict test set labels with classifier
-numtestimages=0
-numcorrect=0
-predicted_clases = []
-
-if not os.path.isfile(filename):
-	for i in range(len(test_images_filenames)):
-	    filename=test_images_filenames[i]
-	    des = fe.extract_single_image_features(filename)
-	    predictedclass = c.predict(des)
-	    predicted_clases.append(predictedclass)
-	    #print('image ' + filename + ' was from class ' + test_labels[i] + ' and was predicted ' + predictedclass)
-	    numtestimages += 1
-	    if predictedclass == test_labels[i]:
-	        numcorrect += 1
-	
-	print 'Saving predicted clases'
-	np.save(filename, predicted_clases)
-	print('Final accuracy: ' + str(numcorrect*100.0/numtestimages))
+# Load precomputed labels if avaliable
+filename = classifier + '_' + feature_method + '.npy'
+if os.path.isfile(filename) and not force_reload:
+    print 'Loading previous predictions'
+    predicted_classes = np.load(filename)
 else:
-	print 'Loading previous predictions'
-	predicted_clases = np.load(filename)
+    start = time.time()
 
-# Performance
-# guardar numcorrect and next es equivalente
-#print('Final accuracy: ' + str(numcorrect*100.0/len(predicted_clases)))
+    print 'Extracting features'
+    fe = FeatureExtractor(feature_method)
+    (X, y) = fe.extract_features(train_images_filenames, train_labels, nimmax=30)
 
-print evaluation.confusionMatrix(test_labels, predicted_clases, True, True)
-precision, recall, fscore, support = evaluation.precision_recall_fscore(test_labels, predicted_clases)
-average_precision, _, _, _ = evaluation.precision_recall_fscore(test_labels, predicted_clases, True)
+    print 'Training a classifier'
+    c = Classifier(classifier)
+    c.fit(X, y)
+
+    print 'Predicting test set labels with the classifier'
+    numtestimages = 0
+    predicted_classes = []
+    for i in range(len(test_images_filenames)):
+        filename = test_images_filenames[i]
+        des = fe.extract_single_image_features(filename)
+        predictedclass = c.predict(des)
+        predicted_classes.append(predictedclass)
+        print('image ' + filename + ' was from class ' + test_labels[i] + ' and was predicted ' + predictedclass)
+        numtestimages += 1
+
+    end = time.time()
+    print('Done in ' + str(end - start) + ' secs.')
+
+    print 'Saving predicted clases'
+    np.save(filename, predicted_classes)
+
+numcorrect = np.sum(predicted_classes == test_labels)
+print('Final accuracy: ' + str(numcorrect * 100.0 / len(predicted_classes)))
+precision, recall, fscore, support = evaluation.precision_recall_fscore(test_labels, predicted_classes)
+average_precision, _, _, _ = evaluation.precision_recall_fscore(test_labels, predicted_classes, True)
 print 'Precision '
 print precision
 print 'Average precision'
 print average_precision
+print evaluation.confusionMatrix(test_labels, predicted_classes, True, True)
 
-#classifier_probabilities = c.predict_proba(predicted_clases)
-#evaluation.plot_roc_curve(test_labels, predicted_clases)
-#evaluation.plot_roc_curve(test_labels, predicted_clases, c, classifier_probabilities)
-#evaluation.plot_roc_curve(test_labels, predicted_clases, c)
-end=time.time()
-print('Done in '+str(end-start)+' secs.')
+# classifier_probabilities = c.predict_proba(predicted_clases)
+# evaluation.plot_roc_curve(test_labels, predicted_clases)
+# evaluation.plot_roc_curve(test_labels, predicted_clases, c, classifier_probabilities)
+# evaluation.plot_roc_curve(test_labels, predicted_clases, c)
