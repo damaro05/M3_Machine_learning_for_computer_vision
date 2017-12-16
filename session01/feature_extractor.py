70b2facae764075ff0589a1f5437b626126d248b
mkmenta <mkmenta95@gmail.com>
2017-12-16 20:13:00 +0100

    Added more feature versions

--- session01/feature_extractor.py
+++ session01/feature_extractor.py
@@ -5,19 +5,69 @@ import os
 import pickle #module for serialization of python object structure
 import gzip   #we can compress our data directly when writting data to a file
 from skimage.feature import hog
+from sklearn.cluster import KMeans
+from skimage.exposure import histogram
+import matplotlib.pyplot as plt
+
 
 class FeatureExtractor:
     def __init__(self, method):
         self.method=method
         if method == 'sift':
             self.f = self._sift_features
+        if method == 'aggsift':
+            self.f = self._aggregated_sift_features
         elif method == 'surf':
             self.f = self._surf_features
         elif method == 'hog':
             self.f = self._hog_features
+        elif method == 'kmeans':
+            self.f = self._kmeans_features
+        elif method == 'histogram':
+            self.f = self._histogram_features
+        elif method == 'aggsift_histogram':
+            self.f = self._aggsift_histogram_features
         else:
             print('Feature extracting method not found')
             sys.exit(-1)
+
+    def _aggsift_histogram_features(self, ima):
+        SIFTdetector = cv2.xfeatures2d.SIFT_create(nfeatures=100)
+        gray = cv2.cvtColor(ima, cv2.COLOR_BGR2GRAY)
+        _, des = SIFTdetector.detectAndCompute(gray, None)
+        #print(str(len(kpt)) + ' extracted keypoints and descriptors')
+        sift = np.mean(des,axis=0)
+
+        hsv = cv2.cvtColor(ima, cv2.COLOR_BGR2HSV)
+        pixels = np.reshape(hsv, (ima.shape[0] * ima.shape[1], 1, 3))
+        pixels = np.squeeze(np.transpose(pixels, (0, 2, 1)))
+        # Color
+        hpixels = pixels[np.logical_and(pixels[:,1] >= 25,pixels[:,2] >= 25),0]
+        hhist,_=np.histogram(hpixels, bins=12)
+        gpixels = pixels[pixels[:, 1] < 25, 2]
+        ghist, _ = np.histogram(gpixels, bins=5)
+
+        return np.concatenate([hhist,ghist,sift])[np.newaxis]
+
+    def _kmeans_features(self, ima):
+        kmeans = KMeans(n_clusters=4)
+        hsv = cv2.cvtColor(ima, cv2.COLOR_BGR2HSV)
+        pixels = np.reshape(hsv, (ima.shape[0]*ima.shape[1], 1, 3))
+        pixels = np.squeeze(np.transpose(pixels, (0, 2, 1)))
+        kmeans.fit(pixels)
+        return np.reshape(kmeans.cluster_centers_,(1,4*3))
+
+    def _histogram_features(self, ima):
+        hsv = cv2.cvtColor(ima, cv2.COLOR_BGR2HSV)
+        pixels = np.reshape(hsv, (ima.shape[0] * ima.shape[1], 1, 3))
+        pixels = np.squeeze(np.transpose(pixels, (0, 2, 1)))
+        # Color
+        hpixels = pixels[np.logical_and(pixels[:,1] >= 25,pixels[:,2] >= 25),0]
+        hhist,_=np.histogram(hpixels, bins=12)
+        gpixels = pixels[pixels[:, 1] < 25, 2]
+        ghist, _ = np.histogram(gpixels, bins=5)
+
+        return np.concatenate([hhist,ghist])[np.newaxis]
 
     def _sift_features(self, ima):
         # SIFTdetector = cv2.SIFT(nfeatures=100)
@@ -26,6 +76,14 @@ class FeatureExtractor:
         kpt, des = SIFTdetector.detectAndCompute(gray, None)
         print(str(len(kpt)) + ' extracted keypoints and descriptors')
         return des
+
+    def _aggregated_sift_features(self, ima):
+        # SIFTdetector = cv2.SIFT(nfeatures=100)
+        SIFTdetector = cv2.xfeatures2d.SIFT_create(nfeatures=100)
+        gray = cv2.cvtColor(ima, cv2.COLOR_BGR2GRAY)
+        kpt, des = SIFTdetector.detectAndCompute(gray, None)
+        print(str(len(kpt)) + ' extracted keypoints and descriptors')
+        return np.mean(des,axis=0)[np.newaxis]
 
     def _surf_features(self, ima):
         SURFdetector = cv2.xfeatures2d.SURF_create(400)
