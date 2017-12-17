Fixed precision,fscore and recall array order

session01/evaluation.py
@@ -51,6 +51,7 @@ def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix'
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
+    plt.savefig('confusionmatrix.png')
     plt.show()
 
 
@@ -80,14 +81,15 @@ def plot_roc_curve(Gtruth, predicted, classifier):
     plt.show()
 
 
-def precision_recall_fscore(Gtruth, predicted, averageP=False):
+def precision_recall_fscore(Gtruth, predicted, average=False):
     """ This function calculates the precision, recall, f-score and support.
         The average precision can be calculated by setting 'averageP'=True """
-    precision, recall, fscore, support = precision_recall_fscore_support(Gtruth, predicted)
-    average_precision = np.mean(precision)
-    if averageP is not False:
-        return average_precision, recall, fscore, support
-    return precision, recall, fscore, support
+    classes = ['mountain', 'inside_city', 'Opencountry', 'coast', 'street', 'forest', 'tallbuilding', 'highway']
+    precision, recall, fscore, support = precision_recall_fscore_support(Gtruth, predicted,labels=classes)
+    if average:
+        return np.mean(precision), np.mean(recall), np.mean(fscore), np.mean(support)
+    else:
+        return precision, recall, fscore, support
 
     
 def plot_precision_recall_curve():

session01/session1.py
@@ -9,8 +9,8 @@ from classifier import Classifier
 
 #######################
 force_reload = False
-feature_method = 'sift'
-classifier = 'bayes'
+feature_method = 'surf'
+classifier = 'knn'
 #######################
 
 # Input images and labels
@@ -57,11 +57,19 @@ else:
 numcorrect = np.sum(np.array(predicted_classes) == np.array(test_labels))
 print('Final accuracy: ' + str(numcorrect * 100.0 / len(predicted_classes)))
 precision, recall, fscore, support = evaluation.precision_recall_fscore(test_labels, predicted_classes)
-average_precision, _, _, _ = evaluation.precision_recall_fscore(test_labels, predicted_classes, True)
+average_precision, average_recall, average_fscore, average_support = evaluation.precision_recall_fscore(test_labels, predicted_classes, True)
 print 'Precision '
-print precision
+print np.round(precision*100,2)
 print 'Average precision'
-print average_precision
+print round(average_precision*100,2)
+print 'Recall '
+print np.round(recall*100,2)
+print 'Average recall'
+print round(average_recall*100,2)
+print 'F1'
+print np.round(fscore*100,2)
+print 'Average F1'
+print round(average_fscore*100,2)
 print evaluation.confusionMatrix(test_labels, predicted_classes, True, True)
 
 # classifier_probabilities = c.predict_proba(predicted_classes)