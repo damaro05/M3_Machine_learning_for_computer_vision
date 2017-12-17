9ef899bbfe70fcaba7286c6337ba1fcfe72ae9bb
damaro05 <scmh05@gmail.com>
2017-12-17 04:27:49 +0100

    Classifiers added: svc and linear svc

--- session01/classifier.py
+++ session01/classifier.py
@@ -2,6 +2,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn import tree
+from sklearn import svm
 import sys
 import numpy as np
 
@@ -28,6 +29,16 @@ class Classifier:
             self.fit = self._tree_fit
             self.predict = self._tree_predict
             self.predict_proba = self._tree_predict_proba
+        elif method == 'svc':
+            self.name = 'support_vector_classification'
+            self.fit = self._svc_fit
+            self.predict = self._svc_predict
+            self.predict_proba = self._svc_predict_proba
+        elif method == 'linearsvc':
+            self.name = 'linear_support_vector_classification'
+            self.fit = self._lsvc_fit
+            self.predict = self._lsvc_predict
+            self.predict_proba = self._lsvc_predict_proba
         else:
             print('Classifying method not found')
             sys.exit(-1)
@@ -93,6 +104,39 @@ class Classifier:
         return values[np.argmax(counts)]
 
     def _tree_predict_proba(self, X):
+        pred_probabilities = self._classifier.predict_proba(X)
+        print 'Predicted probabilities'
+        return pred_probabilities
+
+    def _svc_fit(self, X, y):
+        print('Training the Support Vector classifier...')
+        # ovr = one vs. rest | ovo = one vs. one
+        self._classifier = svm.SVC(decision_function_shape='ovr')
+        self._classifier.fit(X, y)
+        print('Done!')
+
+    def _svc_predict(self, X):
+        predictions = self._classifier.predict(X)
+        values, counts = np.unique(predictions, return_counts=True)
+        return values[np.argmax(counts)]
+
+    def _svc_predict_proba(self, X):
+        pred_probabilities = self._classifier.predict_proba(X)
+        print 'Predicted probabilities'
+        return pred_probabilities
+
+    def _lsvc_fit(self, X, y):
+        print('Training the Linear Support Vector classifier...')
+        self._classifier = svm.LinearSVC()
+        self._classifier.fit(X, y)
+        print('Done!')
+
+    def _lsvc_predict(self, X):
+        predictions = self._classifier.predict(X)
+        values, counts = np.unique(predictions, return_counts=True)
+        return values[np.argmax(counts)]
+
+    def _lsvc_predict_proba(self, X):
         pred_probabilities = self._classifier.predict_proba(X)
         print 'Predicted probabilities'
         return pred_probabilities
