a7c9b28d144fc7b1347d648380b746b3ad834cf2
mkmenta <mkmenta95@gmail.com>
2018-01-11 12:13:39 +0100

    Changed paths

--- code_M3_DL_1/patch_based_mlp_MIT_8_scene.py
+++ code_M3_DL_1/patch_based_mlp_MIT_8_scene.py
@@ -7,8 +7,8 @@ from keras.preprocessing.image import ImageDataGenerator
 #user defined variables
 PATCH_SIZE  = 64
 BATCH_SIZE  = 16
-DATASET_DIR = 'data/MIT_split'
-PATCHES_DIR = 'data/MIT_split_patches'
+DATASET_DIR = '/share/datasets/MIT_split'
+PATCHES_DIR = '/home/master04/data/MIT_split_patches'
 MODEL_FNAME = 'patch_based_mlp.h5'
 
 def build_mlp(input_size=PATCH_SIZE,phase='TRAIN'):
