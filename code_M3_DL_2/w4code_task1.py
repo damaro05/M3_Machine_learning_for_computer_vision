154b5e8965406a7c8095374d6ba94b55d36887f8
mkmenta <mkmenta95@gmail.com>
2018-01-21 20:58:04 +0100

    Reverted

--- code_M3_DL_2/w4code_task1.py
+++ code_M3_DL_2/w4code_task1.py
@@ -23,8 +23,8 @@ test_data_dir = '/share/datasets/MIT_split/test'
 img_width = 224
 img_height = 224
 batch_size = 64
-n_train_samples = 800
-number_of_epoch = 200
+n_train_samples = 400
+number_of_epoch = 20
 script_identifier = 'task_1'
 plot_history = True
 
@@ -69,28 +69,18 @@ base_model = VGG16(weights='imagenet')
 plot_model(base_model, to_file=os.path.join('dump', 'models', 'modelVGG16.png'), show_shapes=True,
            show_layer_names=True)
 
-# Not valid: mem alloc error
+# New hybrid model
 x = base_model.get_layer('block4_pool').output
 x = Conv2D(filters=512, kernel_size=(3, 3), strides=(2, 2))(x)
-
-# x = base_model.get_layer('block4_conv3').output
-# Method 1: 7x7x512 maybe too difficult? doesn't work properly
-# Try with maxpool
-# See what ramon says in the email
-# x = MaxPooling2D(pool_size=(4, 4), strides=(4, 4))(x)  # Strides? Size of pixel jump
-x = Flatten()(x)
-
-# Method 2: maybe too much? only one value per channel i.e. 1x512
-# x = GlobalAveragePooling2D()(x)
-
 x = Dense(4096, activation='relu', name='fc1')(x)
-# x = Dense(4096, activation='relu', name='fc2')(x)
 x = Dense(8, activation='softmax', name='predictions')(x)
-
 model = Model(inputs=base_model.input, outputs=x)
+
 model_identifier = str(hash(str(model.get_config())))
+print('Current model: '+model_identifier)
 plot_model(model, to_file=os.path.join('dump', 'models', script_identifier + '_' + model_identifier + '.png'),
            show_shapes=True, show_layer_names=True)
+
 for layer in base_model.layers:
     layer.trainable = False
 
@@ -126,18 +116,19 @@ validation_generator = datagen.flow_from_directory(val_data_dir,
                                                    batch_size=batch_size,
                                                    class_mode='categorical')
 #first training
-estop_loss=EarlyStopping(monitor='val_loss',min_delta=.25,patience=5,verbose=1)
-estop_acc=EarlyStopping(monitor='val_acc',min_delta=.01,patience=5,verbose=1)
+#estop_loss=EarlyStopping(monitor='val_loss',min_delta=.25,patience=5,verbose=1)
+#estop_acc=EarlyStopping(monitor='val_acc',min_delta=.01,patience=5,verbose=1)
 history = model.fit_generator(train_generator,
                               steps_per_epoch=n_train_samples / batch_size,  # batch_size*(int(400*1881/1881//batch_size)+1)
                               epochs=number_of_epoch,
                               validation_data=validation_generator,
-                              validation_steps=validation_generator.samples / batch_size,
-                              callbacks=[estop_acc,estop_loss])
+                              validation_steps=validation_generator.samples / batch_size)
+                              #callbacks=[estop_acc,estop_loss])
+# save weights
 model.save_weights(os.path.join('dump', 'models',
                                 script_identifier + '_' + model_identifier + '_' + str(batch_size) + '_' + str(
                                     number_of_epoch) + '.h5'))
-
+# save history
 with open(os.path.join('dump', 'histories',
                        script_identifier + '_' + model_identifier + '_' + str(batch_size) + '_' + str(
                            number_of_epoch) + '_history.pklz'),
@@ -146,40 +137,11 @@ with open(os.path.join('dump', 'histories',
         (history.epoch, history.history, history.params, history.validation_data, model.get_config()), f,
         cPickle.HIGHEST_PROTOCOL)
 
-#second training
-for layer in base_model.layers:
-    layer.trainable = True
-
-model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
-for layer in model.layers:
-    print layer.name, layer.trainable
-model.load_weights(os.path.join('dump', 'models',
-                                script_identifier + '_' + model_identifier + '_' + str(batch_size) + '_' + str(
-                                    number_of_epoch) + '.h5'))
-history2 = model.fit_generator(train_generator,
-                              steps_per_epoch=n_train_samples / batch_size,  # batch_size*(int(400*1881/1881//batch_size)+1)
-                              epochs=number_of_epoch,
-                              validation_data=validation_generator,
-                              validation_steps=validation_generator.samples / batch_size,
-                              callbacks=[estop_acc,estop_loss])
-model.save_weights(os.path.join('dump', 'models',
-                                script_identifier + '_' + model_identifier + '_' + str(batch_size) + '_' + str(
-                                    number_of_epoch) + '.h5'))
-
-with open(os.path.join('dump', 'histories',
-                       script_identifier + '_' + model_identifier + '_' + str(batch_size) + '_' + str(
-                           number_of_epoch) + '_history2.pklz'),
-          'wb') as f:
-    cPickle.dump(
-        (history2.epoch, history2.history, history2.params, history2.validation_data, model.get_config()), f,
-        cPickle.HIGHEST_PROTOCOL)
-
 #evaluate
 result = model.evaluate_generator(test_generator)
 print result
 
-# list all data in history
-
+# plot history graphs
 if plot_history:
     # summarize history for accuracy
     plt.plot(history.history['acc'])
